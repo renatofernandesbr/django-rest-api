001f8085a2bd219ed15b393d4784acf64439c9d0
Renato Fernandes <renato.fernandes@serpro.gov.br>
2022-10-10 22:23:49 -0300

    Deploy no Heroku.

--- setup/settings.py
+++ setup/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'rf-django-rest-api-clientes.herokuapp.com'
+]
 
 
 # Application definition
