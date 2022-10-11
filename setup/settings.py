1209083893fd36d46ca60a1c61c6145b95d67e9d
Renato Fernandes <renato.fernandes@serpro.gov.br>
2022-10-11 14:41:51 -0300

    Deploy no Heroku.

--- setup/settings.py
+++ setup/settings.py
@@ -93,13 +93,9 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-rest-api',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
